f0a0aad55e49a9af8ef0e4766634aef96a4d1c75
Abhi Verma <qureshi.nagar34@gmail.com>
2026-07-13 16:35:25 +0530

    new prog

--- letdo.java
+++ letdo.java
@@ -83,3 +83,19 @@ class Solution {
         return arr;
     }
 }
+//removing duplicates
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
