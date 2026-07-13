4b38535e068d88d28f3eebd9fc08ff2a64699bd1
Abhi Verma <qureshi.nagar34@gmail.com>
2026-07-13 16:38:47 +0530

    neww

--- letdo.java
+++ letdo.java
@@ -83,7 +83,8 @@ class Solution {
         return arr;
     }
 }
-//removing duplicates
+
+// removing duplicates
 class Solution {
     public int removeElement(int[] nums, int val) {
 
@@ -99,7 +100,8 @@ class Solution {
         return k;
     }
 }
-//reverse a no
+
+// reverse a no
 class Solution {
     public boolean isPalindrome(int x) {
 
@@ -119,5 +121,43 @@ class Solution {
             return true;
         else
             return false;
+    }
+}
+
+// roman to integer
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int value = 0;
+
+            char ch = s.charAt(i);
+
+            if (ch == 'I')
+                value = 1;
+            else if (ch == 'V')
+                value = 5;
+            else if (ch == 'X')
+                value = 10;
+            else if (ch == 'L')
+                value = 50;
+            else if (ch == 'C')
+                value = 100;
+            else if (ch == 'D')
+                value = 500;
+            else if (ch == 'M')
+                value = 1000;
+
+            if (value < prev)
+                sum = sum - value;
+            else
+                sum = sum + value;
+
+            prev = value;
+        }
+
+        return sum;
     }
 }
